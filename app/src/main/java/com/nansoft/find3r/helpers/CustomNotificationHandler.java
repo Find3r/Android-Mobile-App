7694f5d37b0f1a2f1d71bc3d3610ee611ef98867
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-10-26 19:41:17 -0600

    Se implementa el manejo de varias notificaciones

--- app/src/main/java/com/nansoft/find3r/helpers/CustomNotificationHandler.java
+++ app/src/main/java/com/nansoft/find3r/helpers/CustomNotificationHandler.java
@@ -4,6 +4,7 @@ import com.microsoft.windowsazure.notifications.NotificationsHandler;
 import com.nansoft.find3r.R;
 import com.nansoft.find3r.activity.ComentarioActivity;
 import com.nansoft.find3r.activity.MainActivity;
+import com.nansoft.find3r.activity.NotificationsActivity;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -15,6 +16,8 @@ import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 /**
  * Created by Carlos on 07/10/2015.
  */
@@ -25,13 +28,16 @@ public class CustomNotificationHandler extends NotificationsHandler
     NotificationCompat.Builder builder;
     Context ctx;
 
+    ArrayList <String> collectionNotifications = new ArrayList<String>();
+
     @Override
     public void onReceive(Context context, Bundle bundle) {
         ctx = context;
         String idNoticia = bundle.getString("id");
         String nhMessage = bundle.getString("message");
+        collectionNotifications.add(nhMessage);
 
-        sendNotification(idNoticia,nhMessage);
+        sendNotification(idNoticia, nhMessage);
     }
 
     private void sendNotification(String idNoticia,String msg) {
@@ -41,23 +47,53 @@ public class CustomNotificationHandler extends NotificationsHandler
                 ctx.getSystemService(Context.NOTIFICATION_SERVICE);
 
 
-        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0,
-                new Intent(ctx, ComentarioActivity.class).putExtra("idNoticia",idNoticia), 0);
+        PendingIntent contentIntent;
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(ctx)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("Find3r")
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(msg))
-                        .setContentText(msg);
+                        .setSmallIcon(R.mipmap.ic_launcher);
+
+        NotificationCompat.InboxStyle inBoxStyle =
+                new NotificationCompat.InboxStyle();
+
+        int tamanio = collectionNotifications.size();
+
+        String mensajeAux = tamanio + " interacción";
+
+        if (tamanio > 1)
+        {
+            mensajeAux = tamanio + " interacciones";
+            contentIntent = PendingIntent.getActivity(ctx, 0,
+                    new Intent(ctx, NotificationsActivity.class).putExtra("idNoticia", idNoticia), 0);
+
+        }
+        else
+        {
+            contentIntent = PendingIntent.getActivity(ctx, 0,
+                    new Intent(ctx, ComentarioActivity.class).putExtra("idNoticia", idNoticia), 0);
+        }
+        mBuilder.setContentIntent(contentIntent);
+
+        inBoxStyle.setBigContentTitle(mensajeAux);
+
+        for (int i=0; i < tamanio ; i++) {
+
+            inBoxStyle.addLine(collectionNotifications.get(i));
+        }
+        // Moves the expanded layout object into the notification object.
+        mBuilder.setStyle(inBoxStyle);
+
+
+
 
         mBuilder.setAutoCancel(true);
 
         mBuilder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
 
-        mBuilder.setContentIntent(contentIntent);
+       // mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
+
+
 
 }
