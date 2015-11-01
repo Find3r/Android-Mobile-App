ebc21d3dec0618926775958fe3e84e3c153d652d
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-11-01 09:35:54 -0600

    Se agrega layout que contiene la descripción de la noticia con un fondo negro con cierta opacidad para que el texto sea legible :D

--- app/src/main/java/com/nansoft/find3r/activity/NewDescriptionActivity.java
+++ app/src/main/java/com/nansoft/find3r/activity/NewDescriptionActivity.java
@@ -2,6 +2,7 @@ package com.nansoft.find3r.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -17,6 +18,15 @@ import uk.co.senab.photoview.PhotoView;
 
 public class NewDescriptionActivity extends CustomAppCompatActivity {
 
+    SwipeRefreshLayout swipeRefreshLayout;
+    TextView txtvNewName;
+    TextView txtvNewDescription;
+    PhotoView imgvNewDescription;
+
+    TextView txtvEstado;
+    ImageView imgvEstado;
+
+    NoticiaCompleta objNoticiaCompleta;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -25,46 +35,22 @@ public class NewDescriptionActivity extends CustomAppCompatActivity {
         setContentView(R.layout.new_description);
 
 
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swprlNewDescription);
+        txtvNewName = (TextView) findViewById(R.id.txtvNewName);
+        txtvNewDescription = (TextView) findViewById(R.id.txtvNewDescription);
+        imgvNewDescription = (PhotoView) findViewById(R.id.imgvNewDescription);
 
-        TextView txtvNewName = (TextView) findViewById(R.id.txtvNewName);
-        TextView txtvNewDescription = (TextView) findViewById(R.id.txtvNewDescription);
-        PhotoView imgvNewDescription = (PhotoView) findViewById(R.id.imgvNewDescription);
-
-        TextView txtvEstado = (TextView) findViewById(R.id.txtvEstado);
-        ImageView imgvEstado = (ImageView) findViewById(R.id.imgvEstado);
+        txtvEstado = (TextView) findViewById(R.id.txtvEstado);
+        imgvEstado = (ImageView) findViewById(R.id.imgvEstado);
 
         txtvNewDescription.setMovementMethod(new ScrollingMovementMethod());
 
         try {
 
-            //String urlImagen = getIntent().getExtras().getString("URL_IMAGEN");
-            final NoticiaCompleta objNoticiaCompleta = (NoticiaCompleta) getIntent().getParcelableExtra("obj");
 
-            txtvNewName.setText(objNoticiaCompleta.getNombre());
+            objNoticiaCompleta = (NoticiaCompleta) getIntent().getParcelableExtra("obj");
 
-            txtvNewDescription.setText(objNoticiaCompleta.getDescripcion());
-
-            Glide.with(this)
-                    .load(objNoticiaCompleta.getUrlImagen().trim())
-                    .asBitmap()
-                    .fitCenter()
-                    .placeholder(R.drawable.picture_default)
-                    .error(R.drawable.error_image)
-                    .into(imgvNewDescription);
-
-            if(objNoticiaCompleta.getIdestado().trim().equals("0"))
-            {
-                txtvEstado.setText(getString(R.string.lost));
-                imgvEstado.setImageResource(getResources().getIdentifier("lost","drawable", getPackageName()));
-
-            }
-            else
-            {
-                txtvEstado.setText(getString(R.string.found));
-                imgvEstado.setImageResource(getResources().getIdentifier("found","drawable", getPackageName()));
-
-            }
-
+            cargarDatos();
 
             findViewById(R.id.imgvComentario).setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -80,7 +66,43 @@ public class NewDescriptionActivity extends CustomAppCompatActivity {
 
         }
 
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                cargarDatos();
+            }
+        });
 
 
+    }
+
+    private void cargarDatos()
+    {
+        txtvNewName.setText(objNoticiaCompleta.getNombre());
+
+        txtvNewDescription.setText(objNoticiaCompleta.getDescripcion());
+
+        Glide.with(this)
+                .load(objNoticiaCompleta.getUrlImagen().trim())
+                .asBitmap()
+                .fitCenter()
+                .placeholder(R.drawable.picture_default)
+                .error(R.drawable.error_image)
+                .into(imgvNewDescription);
+
+        if(objNoticiaCompleta.getIdestado().trim().equals("0"))
+        {
+            txtvEstado.setText(getString(R.string.lost));
+            imgvEstado.setImageResource(getResources().getIdentifier("lost","drawable", getPackageName()));
+
+        }
+        else
+        {
+            txtvEstado.setText(getString(R.string.found));
+            imgvEstado.setImageResource(getResources().getIdentifier("found","drawable", getPackageName()));
+
+        }
+
+        swipeRefreshLayout.setRefreshing(false);
     }
 }
